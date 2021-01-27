66639fdf16fc05a966dbac34d7e158e06ccfa0f6
Björn Strömberg <bjorn.stromberg@codic.se>
2021-01-27 09:42:06 +0100

    Abs link update

--- JS-lecture-challenges/js/component.js
+++ JS-lecture-challenges/js/component.js
@@ -4,17 +4,17 @@ let component = {
 
     document.head.insertAdjacentHTML(
       "beforeend",
-      `<link rel="stylesheet" href="/css/components/${name}.css"/>`
+      `<link rel="stylesheet" href="/JS-lecture-challenges/css/components/${name}.css"/>`
     );
 
-    fetch(`/html/components/${name}.html`)
+    fetch(`/JS-lecture-challenges/html/components/${name}.html`)
       .then((response) => response.text())
       .then((text) => {
         spot.outerHTML = text;
 
         let script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = `/js/components/${name}.js`;
+        script.src = `/JS-lecture-challenges/js/components/${name}.js`;
         document.head.append(script);
       });
   },
