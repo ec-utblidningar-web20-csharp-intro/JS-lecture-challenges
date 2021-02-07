807d83992c471d057efb545b5f1accc4a97f2c3c
Björn Strömberg <bjorn.stromberg@codic.se>
2021-02-07 17:00:37 +0100

    Temp commit mitten av AJAX frågor

--- js/pages/ajax_frågor.js
+++ js/pages/ajax_frågor.js
@@ -5,13 +5,13 @@
   */
   let url = new URL("https://api.openweathermap.org/data/2.5/weather");
 
-  url.searchParams.set("q", "Varberg,Sweden");
-  url.searchParams.set("appid", "din_hashkod_här");
-  url.searchParams.set("mode", "html");
-  url.searchParams.set("units", "metric");
-  url.searchParams.set("lang", "se");
+  url.searchParams.append("q", "Varberg,Sweden");
+  url.searchParams.append("appid", "ac5d516646126253361022bafa972296");
+  url.searchParams.append("mode", "json");
+  url.searchParams.append("units", "metric");
+  url.searchParams.append("lang", "se");
 
-  for(let [key, val] in url.searchParams){
+  for (let [key, val] of url.searchParams) {
     console.log(`${key}:${val}`);
   }
 
@@ -19,16 +19,89 @@
   /* 
    [Fråga 2]
   */
+  {
+    let out = document.querySelector("#q2_out");
+    let xhr = new XMLHttpRequest();
 
-  let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "json";
 
-  xhr.open("GET", url);
-  xhr.send();
+    xhr.onload = function (e) {
+      //'e.target' kommer här att peka på XMLHttpRequest objektet
+      console.log(
+        `Weather request status: ${e.target.status} ${e.target.statusText}`
+      );
 
-  xhr.onload(function(){
+      if (e.target.status === 200) {
+        // Begäran lyckades
+        let jsObjekt = e.target.response;
+        out.innerText = JSON.stringify(jsObjekt, null, " ");
+      }
+    };
 
-  });
+    xhr.onerror = function (e) {
+      console.log(url.origin + " could not be reached");
+    };
+
+    xhr.send();
+  }
   /* 
    [Fråga 3]
+  */
+  {
+    let ball = document.querySelector("#q3 .ball");
+    //ball.addEventListener("click", moveSequence, { once: true });
+
+    function moveSequence(e) {
+      ball.style.animation = "m1 1s 1";
+
+      setTimeout(function () {
+        ball.style.animation = "m2 1s 1";
+
+        setTimeout(function () {
+          ball.style.animation = "m3 1s 1";
+
+          setTimeout(function () {
+            ball.style.animation = "m4 1s 1";
+
+            setTimeout(function () {
+              ball.addEventListener("click", moveSequence, { once: true });
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }
+  }
+  /* 
+   [Fråga 4]
+  */
+  {
+    let ball = document.querySelector("#q3 .ball");
+    ball.addEventListener("click", moveSequence, { once: true });
+
+    async function moveSequence(e) {
+      ball.style.animation = "m1 1s 1";
+      await defer(1000);
+
+      ball.style.animation = "m2 1s 1";
+      await defer(1000);
+
+      ball.style.animation = "m3 1s 1";
+      await defer(1000);
+
+      ball.style.animation = "m4 1s 1";
+      await defer(1000);
+      
+      ball.addEventListener("click", moveSequence, { once: true });
+    }
+
+    function defer(ms){
+      return new Promise(function(resolve) {
+        setTimeout(resolve, ms);
+      });
+    }
+  }
+  /* 
+   [Fråga 5]
   */
 }
--- js/pages/ajax_övning.js
+++ js/pages/ajax_övning.js
@@ -5,13 +5,19 @@
   |            inför Fråga 1             |
   ---------------------------------------- 
   */
-  
   /* 
   ________________________________________
   |            inför Fråga 2             |
   ---------------------------------------- 
   */
-  
+  // HTTP respons koder
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+
+  // Alla möjliga events för XMLHttpRequest som man kan registrera
+  // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequestEventTarget
+
+  // progress objekt som skickas med som parameter
+  // https://developer.mozilla.org/en-US/docs/Web/API/ProgressEvent
   /* 
   ________________________________________
   |            inför Fråga 3             |
