4a0d4ef495d6f41ef5c2a1fa6922271bdd41e176
Björn Strömberg <bjorn.stromberg@codic.se>
2021-02-09 10:32:46 +0100

    lite om url i övning

--- js/pages/ajax/övning.js
+++ js/pages/ajax/övning.js
@@ -2,10 +2,49 @@
 {
   /* 
   ________________________________________
-  |            inför Fråga 1             |
+  |            inför Fråga 1  (URL)      |
   ---------------------------------------- 
   */
-  /* 
+  // href -- hela adressen
+
+  // origin -- allt om metod(protokoll) & 
+  //           vart adressen gäller(hostname, port)
+
+  // host -- hostname + port
+  // hostname -- domän/website namn eller ip-adress (ipv4,ipv6)
+  // port -- sifferkod för vilken _app_ på en server/PC du pratar med
+
+  // protocol -- vad är det vi håller på med 
+  //  (hämta hypertext? HTTP, hämta fil online? FTP, hämta fil lokalt? file)
+
+  // pathname -- vart, i en fil&folder struktur ligger X
+  // search -- parametrar till en WebAPI metod
+  // hash -- fragment (en bit av) det du söker. Element id i html doc
+  
+  // Börja leka med googles search api
+  // https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters
+  function objProp2SearchParam(url, obj){
+    for(const key in obj){
+      url.searchParams.append(key, obj[key]);
+    }
+  }
+  
+  const url = new URL("https://google.com/search");
+  
+  const mySearch = {
+    q: "bilar",
+    num: 2,
+  };
+  
+  objProp2SearchParam(url, mySearch);
+
+  // loopa igenom parametrar
+  let props = url.searchParams.entries();
+
+  for(const [k, v] of props){
+    console.log(k + ": " + v);
+  }
+  /*
   ________________________________________
   |            inför Fråga 2             |
   ---------------------------------------- 
